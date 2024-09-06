39717a0cbeeddd5a6f19fae1746a5d3f952e600c
Jishnu Khargharia <jishnukhargharia11@gmail.com>
2024-09-06 19:57:49 +0530

    skill update

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,61 +1,130 @@
-import React from 'react'
-import './Skills.css'
+import React from 'react';
+import './Skills.css';
+import cpp from '../../images/c++.png'
+import c from '../../images/c.png'
+import py from '../../images/py.png'
+import js from '../../images/js.png'
+import ts from '../../images/ts.png'
+import re from '../../images/re.png'
+import tw from '../../images/tw.png'
+import html from '../../images/html.png'
+import css from '../../images/css.png'
+import nd from '../../images/nd.png'
+import ex from '../../images/ex.png'
+import mongoose from '../../images/mongoose.png'
+import mdb from '../../images/mdb.png'
+import gt from '../../images/gt.png'
+import gth from '../../images/gth.png'
+import fgm from '../../images/fgm.png'
+import fra from '../../images/fra.png'
 
 function Skills() {
   return (
     <section id="skills" className='skills h-fit py-16'>
       <div className=' h-fit flex p-20 justify-center'>
         <div className='text-center'>
-          <h1 className='text-6xl mb-5'>Tech Stacks</h1>
+          <h1 className='text-6xl mb-7'>Tech Stacks</h1>
           <p className='text-2xl'>Here are some of the skills that I have been working on in the past year.</p>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
-            <div className="card  p-4 text-center">
-              <h1 className='hh text-3xl'>Languages</h1>
-              <div className='sp grid grid-cols-1 sm:grid-cols-4 gap-3 p-3'>
-                <span>C++</span>
-                <span>C</span>
-                <span>Javascript</span>
-                <span>TypeScript</span>
-                <span>Python</span>
+          <div className="mt-14">
+
+            <div className="languages lang flex">
+              <h1>Languages : </h1>
+              <div>
+                <div className='dd flex'>
+                  <img src={cpp} alt="" />
+                  <span>C++</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={c} alt="" />
+                  <span>C</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={py} alt="" />
+                  <span>Python</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={js} alt="" />
+                  <span>JavaScript</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={ts} alt="" />
+                  <span>TypeScript</span>
+                </div>
               </div>
             </div>
-            <div className="card  p-4 text-center">
-              <h1 className='hh text-3xl'>Frontend</h1>
-              <div className='sp grid grid-cols-1 sm:grid-cols-4 gap-3 justify-center p-3'>
-                <span>React</span>
-                <span>Tailwind Css</span>
-                <span>React Scroll</span>
-                <span>Bootstrap</span>
-                <span>HTML</span>
-                <span>CSS</span>
-                <span className='w-44'>React Router Dom</span>
+
+            <div className="frontend lang flex">
+              <h1>Frontend : </h1>
+              <div>
+                <div className='dd flex'>
+                  <img src={re} alt="" />
+                  <span>React</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={tw} alt="" />
+                  <span>Tailwind CSS</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={re} alt="" />
+                  <span>React Router Dom</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={re} alt="" />
+                  <span>React Scroll</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={html} alt="" />
+                  <span>HTML</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={css} alt="" />
+                  <span>CSS</span>
+                </div>
               </div>
             </div>
-            <div className="card  p-4 text-center">
-              <h1 className='hh text-3xl'>Backend</h1>
-              <div className='sp grid grid-cols-1   sm:grid-cols-4 gap-3 p-3'>
-                <span>Node</span>
-                <span>Express</span>
-                <span>Axios</span>
-                <span>Mongoose</span>
+
+            <div className="backend lang flex">
+              <h1>Backend : </h1>
+              <div>
+                <div className='dd flex'>
+                  <img src={nd} alt="" />
+                  <span>Node js</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={ex} alt="" />
+                  <span>Express</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={mongoose} alt="" />
+                  <span>Mongoose</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={mdb} alt="" />
+                  <span>MongoDb</span>
+                </div>
               </div>
             </div>
-            <div className="card  p-4 text-center">
-              <h1 className='hh text-3xl'>Databases</h1>
-              <div className='sp grid grid-cols-1 sm:grid-cols-4 gap-3 p-3'>
-                <span>MySQL</span>
-                <span>MongoDB</span>
-              </div>
-            </div>
-            <div className="card  p-4 text-center">
-              <h1 className='hh text-3xl'>Dev Tools</h1>
-              <div className='sp grid grid-cols-1 sm:grid-cols-4 gap-3 p-3'>
-                <span>Git</span>
-                <span>GitHub</span>
-                <span>Framer Motion</span>
-                <span>Figma</span>
-                <span>Vercel</span>
+
+            <div className="others flex lang">
+              <h1>Dev Tools : </h1>
+              <div>
+              <div className='dd flex'>
+                  <img src={gt} alt="" />
+                  <span>Git</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={gth} alt="" />
+                  <span>GitHub</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={fgm} alt="" />
+                  <span>Figma</span>
+                </div>
+                <div className='dd flex'>
+                  <img src={fra} alt="" />
+                  <span>Framer Motion</span>
+                </div>
               </div>
             </div>
 
